33c05631bee6ec3254543ebf93e8fefaaf10cc93
Zhanybek Mamataliev <43249178+mamataliev@users.noreply.github.com>
2018-11-04 15:32:06 -0600

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "joni" {
 	ami = "${lookup(var.amis, var.region)}"
 	instance_type = "${var.instancetype}"
-	key_name = "yes"
+	key_name = "${var.SSHKEY"
 	associate_public_ip_address = "true"
 	count = "${var.count}"
 }
